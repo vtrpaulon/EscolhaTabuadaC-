8be3ca8f069555d1adbf3dd19030217b3e9d7f32
Vitor Paulon <vtrpaulon@gmail.com>
2024-01-18 16:14:31 +0000

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace EscolhaTabuada
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+        } 
+    }
+}
